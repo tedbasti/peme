98b61e02bd447b30828e86341a2ace7f7d0b3b41
tedbasti <tedbasti@gmx.de>
2020-02-14 20:48:00 +0100

    Unfinished implementation of parser
    The parser does:
    - Act as everything non bracket is an atom
    --> Need to check if its a string, number or anything else
    - Isnt able to check when there are to many closed brackets
      (One of the little errors that could happen within lisp code)

--- parser.py
+++ parser.py
@@ -4,10 +4,54 @@ from __future__ import print_function
 # This is a class to store every element
 # and its value
 class Element:
-    def __init__(self, type, value):
-        self.type = type
+    def __init__(self, dtype, value):
+        self.dtype = dtype
         self.value = value
 
+    #Equals to check with == (e.q. for the tests)
+    def __eq__(self, other):
+        return type(other) == Element \
+               and self.dtype == other.dtype \
+               and self.value == other.value
 
-def parser(tokens):
-    return []
+    #not equals
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    #toString() method
+    def __str__(self):
+        return "dtype: " + self.dtype + ", value " + self.value
+
+    #Seems like within lists this is called instead of __str__()
+    def __repr__(self):
+        return self.__str__()
+
+
+class TYPES:
+    INTEGER = 1
+    STRING = 2
+    ATOM = 3
+
+
+class ParserException(Exception):
+    pass
+
+
+def parser(tokens, pos=0, depth=0):
+    result = []
+    while pos < len(tokens):
+        if tokens[pos] == "(":
+            subresult, pos = parser(tokens, pos + 1, depth+1)
+            result.append(subresult)
+        elif tokens[pos] == ")":
+            pos+=1
+            if depth == 0 and pos < len(tokens):
+                raise ParserException("To many closed brackets")
+            return result, pos + 1
+        else: #This is a 'real' object for sure!
+            #At the moment everything is just an atom.
+            #TODO: here need to be checked, if this is an
+            #atom, a string, a number or whatever is possible
+            result.append(Element(TYPES.ATOM, tokens[pos]))
+            pos += 1
+    return result
--- parser_test.py
+++ parser_test.py
@@ -1,9 +1,23 @@
 import unittest
 from parser import parser
+from parser import Element
+from parser import TYPES
+
 
 class MyTestCase(unittest.TestCase):
     def test_empty_token(self):
-        self.assertEqual(parser([]), [])
+        self.assertEqual([], parser([]))
+
+    def test_atom_object(self):
+        self.assertEqual([Element(TYPES.ATOM, 'atom')],
+                         parser(['atom']))
+
+    def test_atom_object_brackets(self):
+        self.assertEqual([[Element(TYPES.ATOM, 'atom')]],
+                         parser(['(', 'atom', ')']))
+
+    def test_wrong_brackets(self):
+        parser(['(', ')', ')', ')'])
 
 
 if __name__ == '__main__':
