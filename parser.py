0a84d6a8377a80d0a216ca3e52b99de2f93d0a9e
tedbasti <tedbasti@gmx.de>
2020-02-17 00:01:02 +0100

    Adapt parser to the new logic and adapt tests

--- parser.py
+++ parser.py
@@ -1,57 +1,24 @@
 from __future__ import print_function
 
 
-# This is a class to store every element
-# and its value
-class Element:
-    def __init__(self, dtype, value):
-        self.dtype = dtype
-        self.value = value
-
-    # Equals to check with == (e.q. for the tests)
-    def __eq__(self, other):
-        return type(other) == Element \
-               and self.dtype == other.dtype \
-               and self.value == other.value
-
-    # not equals
-    def __ne__(self, other):
-        return not self.__eq__(other)
-
-    # toString() method
-    def __str__(self):
-        #return "(dtype: " + str(self.dtype) + ", value " + self.value + ")"
-        return self.value
-
-    # Seems like within lists this is called instead of __str__()
-    def __repr__(self):
-        return self.__str__()
-
-
-class TYPES:
-    INTEGER = 1
-    STRING = 2
-    ATOM = 3
-
-
 class ParserException(Exception):
     pass
 
 
-def parser(tokens, pos=0, depth=0):
+def parser(tokens):
+    return parserInternal(tokens, 0, 0)[0]
+
+def parserInternal(tokens, pos, depth):
     result = []
     while pos < len(tokens):
-        if tokens[pos] == "(":
-            subresult, pos = parser(tokens, pos + 1, depth+1)
+        if tokens[pos][0] == "(":
+            subresult, pos = parserInternal(tokens, pos + 1, depth+1)
             result.append(subresult)
-        elif tokens[pos] == ")":
+        elif tokens[pos][0] == ")":
             if depth == 0:
                 raise ParserException("To many closed brackets")
             return result, pos + 1
         else: #This is a 'real' object for sure!
-            #At the moment everything is just an atom.
-            #TODO: here need to be checked, if this is an
-            #atom, a string, a number or whatever is possible
-            result.append(Element(TYPES.ATOM, tokens[pos]))
+            result.append(tokens[pos])
             pos += 1
     return result, pos
--- parser_test.py
+++ parser_test.py
@@ -4,28 +4,29 @@ from parser import *
 
 class MyTestCase(unittest.TestCase):
     def test_empty_token(self):
-        self.assertEqual([], parser([])[0])
+        self.assertEqual([], parser([]))
 
     def test_atom_object(self):
-        self.assertEqual([Element(TYPES.ATOM, 'atom')],
-                         parser(['atom'])[0])
+        self.assertEqual([("identifier", "atom")],
+                         parser([("identifier", "atom")]))
 
     def test_atom_object_brackets(self):
-        self.assertEqual([[Element(TYPES.ATOM, 'atom')]],
-                         parser(['(', 'atom', ')'])[0])
+        self.assertEqual([[("identifier", "atom")]],
+                         parser([("(", ""), ("identifier", "atom"), (")", "")]))
 
     def test_wrong_brackets(self):
         with self.assertRaises(ParserException):
             parser(['(', ')', ')'])
 
     def test_huge(self):
-        result,_ = parser(['(', 'defun', '(', 'double', 'x', ')', '(', '+', 'x', 'x', ')', ')'])
-        self.assertEqual(
-            [[Element(TYPES.ATOM, 'defun'),
-             [Element(TYPES.ATOM, 'double'), Element(TYPES.ATOM, 'x')],
-             [Element(TYPES.ATOM, '+'), Element(TYPES.ATOM, 'x'), Element(TYPES.ATOM, 'x')]]]
-            ,
-            result)
+        # tokenize("(defun (double x) (+ x x))")) # Just to see the original code snippet
+        result = parser([("(", ""), ("identifier", "defun"), ("(", ""), ("identifier", "double"), ("identifier", "x"),
+         (")", ""), ("(", ""), ("identifier", "+"), ("identifier", "x"), ("identifier", "x"), (")", ""),
+         (")", "")])
+        self.assertEqual([[("identifier", "defun"),
+                           [("identifier", "double"), ("identifier", "x")],
+                           [("identifier", "+"), ("identifier", "x"), ("identifier", "x")]]]
+            ,result)
 
 
 if __name__ == '__main__':
