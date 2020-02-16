f64fab73da16b0ef279847e7b66181ba2fccfc47
tedbasti <tedbasti@gmx.de>
2020-02-16 22:31:47 +0100

    Completely rewrite lexer
    The old lexer was really poor and didnt realize much. It just splits some
    strings.
    The new one is more like the well known lexar in the form of a touple
    ("description", "value").

--- tokenizer.py
+++ tokenizer.py
@@ -2,27 +2,36 @@ from __future__ import print_function
 import re
 import sys
 
-def tokenize(code):
-    #Put a space around all brackets and split it
-    return re.sub(r"(\(|\))", r" \1 ", code).split()
+class Lexer:
+    def __init__(self, regex, name, valueNeeded=True, skip=False):
+        self.regex = regex # the regex to be used
+        self.name = name # The name of the token (--> "string" <--, "Michael")
+        self.valueNeeded = valueNeeded # Is the value of the token needed?
+        self.skip = skip # Skip the fields (whitespaces are skipped
 
-def old_tokenize(code):
+
+def tokenize(code):
     result = []
     pos = 0
-    lexar = {
-        "open brackets": re.compile('\('),
-        "close brackets": re.compile('\)'),
-        "whitespace": re.compile('\s+'),
-        "nowhitespace": re.compile('[^\s\(\)]+')
-    }
+    lexer = [
+        Lexer(re.compile('\('), "(", False), # Open brackets
+        Lexer(re.compile('\)'), ")", False), # Closing brackets
+        Lexer(re.compile('".*"'), "string"), # Strings "a b c d"
+        Lexer(re.compile('\s+'), "whitespace", False, True), # Skip whitespaces
+        Lexer(re.compile('[a-zA-Z\+\-\/\*][a-zA-Z0-9\+\-\/\*]*'), "identifier"), # identifier
+        Lexer(re.compile('[0-9.]+'), "number"),  # numbers
+    ]
 
     while pos < len(code):
         posbefore = pos
-        for key, l in lexar.items():
-            r = l.match(code[pos:])
+        for l in lexer:
+            r = l.regex.match(code[pos:])
             if r:
-                if key != "whitespace":
-                    result.append(r.group())
+                if not l.skip:
+                    value = ""
+                    if (l.valueNeeded):
+                        value = r.group()
+                    result.append((l.name, value))
                 pos += r.end()
                 break
         # When the position doesnt change,
--- tokenizer_test.py
+++ tokenizer_test.py
@@ -6,31 +6,32 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual([], tokenize(""))
 
     def test_bracket_open(self):
-        self.assertEqual(['('], tokenize("("))
+        self.assertEqual([("(", "")], tokenize("("))
 
     def test_bracket_open_double(self):
-        self.assertEqual(['(', '('], tokenize("(("))
+        self.assertEqual([("(", ""), ("(", "")], tokenize("(("))
 
     def test_bracket_close(self):
-        self.assertEqual([')'], tokenize(")"))
+        self.assertEqual([(")", "")], tokenize(")"))
 
     def test_bracket_close(self):
-        self.assertEqual([')', ')'], tokenize("))"))
+        self.assertEqual([(")", ""), (")", "")], tokenize("))"))
 
     def test_bracket_open_close(self):
-        self.assertEqual(['(', ')'], tokenize("()"))
+        self.assertEqual([("(", ""), (")", "")], tokenize("()"))
 
     def test_bracket_close_open(self):
-        self.assertEqual([')', '('], tokenize(")("))
+        self.assertEqual([(")", ""), ("(", "")], tokenize(")("))
 
     def test_numeric_atom(self):
-        self.assertEqual(['1234'], tokenize("1234"))
+        self.assertEqual([("number", "1234")], tokenize("1234"))
 
     def test_character_atom(self):
-        self.assertEqual(['character'], tokenize("character"))
+        self.assertEqual([("identifier", "character")], tokenize("character"))
 
     def test_space_end_and_beginning(self):
-        self.assertEqual(['(', '+', '2', '2', ')'], tokenize(" (+ 2 2) "))
+        self.assertEqual([("(", ""), ("identifier", "+"), ("number", "2"), ("number", "2"), (")", "")],
+                         tokenize(" (+ 2 2) "))
 
     def test_complex_code(self):
         self.assertEqual(
