9aee89581519454c8ea36bbef09d64d85667b9d6
tedbasti <tedbasti@gmx.de>
2020-03-14 00:54:39 +0100

    Fix wrong test

--- peme_test.py
+++ peme_test.py
@@ -3,7 +3,7 @@ from peme import execute
 
 class MyTestCase(unittest.TestCase):
     def test_basic(self):
-        self.assertEqual(15, execute("(+ 1 13 1)"))
+        self.assertEqual(15, execute("(+ 1 13 1)")[0])
 
 
 if __name__ == '__main__':
